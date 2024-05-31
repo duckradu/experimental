cec3ba72c124eeafdf7376e72e9d7dea59716bca
2blackholes2 <radu.dascalu97@gmail.com>
2024-05-31 15:05:58 +0100

    Add solid-devtools plugin to app.config

--- apps/monolith/app.config.ts
+++ apps/monolith/app.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "@solidjs/start/config";
+import devtools from "solid-devtools/vite";
 import UnoCSS from "unocss/vite";
 
 export default defineConfig({
   vite: {
-    plugins: [UnoCSS()],
+    plugins: [
+      devtools({
+        autoname: true,
+        locator: {
+          targetIDE: "vscode",
+          key: "Alt",
+          jsxLocation: true,
+          componentLocation: true,
+        },
+      }),
+      UnoCSS(),
+    ],
   },
 });
