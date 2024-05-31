17efc1b7115fd4f5540ac53912d12f7eeb91caf6
2blackholes2 <radu.dascalu97@gmail.com>
2024-05-31 14:58:18 +0100

    Set up transformers & presets

--- apps/monolith/uno.config.ts
+++ apps/monolith/uno.config.ts
@@ -1,3 +1,29 @@
-import { defineConfig } from "unocss";
+import {
+  defineConfig,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
+import presetAnimations from "unocss-preset-animations";
 
-export default defineConfig({});
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "bunny",
+      fonts: {
+        sans: {
+          name: "Noto Sans",
+          weights: ["400", "500", "600", "700", "800", "900"],
+        },
+        // logo: "Lakki Reddy",
+        // logo: "Aladin",
+        // logo: "Dokdo",
+        // logo: "East Sea Dokdo",
+      },
+    }),
+    presetAnimations(),
+  ],
+  transformers: [transformerVariantGroup(), transformerDirectives()],
+});
