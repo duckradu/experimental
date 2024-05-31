c61107f3f7ff7d80ddd460b452599db2ce1e63d1
2blackholes2 <radu.dascalu97@gmail.com>
2024-05-31 15:05:46 +0100

    Port css config

--- apps/monolith/uno.config.ts
+++ apps/monolith/uno.config.ts
@@ -26,4 +26,55 @@ export default defineConfig({
     presetAnimations(),
   ],
   transformers: [transformerVariantGroup(), transformerDirectives()],
+  theme: {
+    colors: {
+      background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
+
+      muted: {
+        DEFAULT: "hsl(var(--muted))",
+        foreground: "hsl(var(--muted-foreground))",
+      },
+
+      popover: {
+        DEFAULT: "hsl(var(--popover))",
+        foreground: "hsl(var(--popover-foreground))",
+      },
+
+      primary: {
+        DEFAULT: "hsl(var(--primary))",
+        foreground: "hsl(var(--primary-foreground))",
+      },
+      secondary: {
+        DEFAULT: "hsl(var(--secondary))",
+        foreground: "hsl(var(--secondary-foreground))",
+      },
+
+      brand: {
+        DEFAULT: "hsl(var(--brand))",
+        foreground: "hsl(var(--brand-foreground))",
+      },
+
+      accent: {
+        DEFAULT: "hsl(var(--accent))",
+        foreground: "hsl(var(--accent-foreground))",
+      },
+
+      destructive: {
+        DEFAULT: "hsl(var(--destructive))",
+        foreground: "hsl(var(--destructive-foreground))",
+      },
+
+      border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
+      ring: "hsl(var(--ring))",
+    },
+    borderRadius: {
+      sm: "calc(var(--radius) - 4px)",
+      md: "calc(var(--radius) - 2px)",
+      lg: "var(--radius)",
+      xl: "calc(var(--radius) + 2px)",
+      "2xl": "calc(var(--radius) + 4px)",
+    },
+  },
 });
