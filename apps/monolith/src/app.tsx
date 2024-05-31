3ec441eef076dc9f063722fdf07e6ae27d6e2b46
2blackholes2 <radu.dascalu97@gmail.com>
2024-05-31 15:08:17 +0100

    Install @unocss/reset

--- apps/monolith/src/app.tsx
+++ apps/monolith/src/app.tsx
@@ -3,7 +3,7 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 
-// import "@unocss/reset/tailwind-compat.css";
+import "@unocss/reset/tailwind-compat.css";
 
 import "virtual:uno.css";
 
