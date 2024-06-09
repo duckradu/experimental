5411e3e09133c31d7cb7d60e1c786201388eff33
2blackholes2 <radu.dascalu97@gmail.com>
2024-06-09 13:04:19 +0100

    Add logo icon

--- apps/monolith/src/components/ui/icon.tsx
+++ apps/monolith/src/components/ui/icon.tsx
@@ -1,7 +1,10 @@
+import LogoSolid from "~icons/local/logo-solid";
 import SolarHomeSmileBold from "~icons/solar/home-smile-bold";
 import SolarHomeSmileOutline from "~icons/solar/home-smile-outline";
 
 export const Icon = {
+  logo: { solid: LogoSolid },
+
   home: {
     smile: {
       outline: SolarHomeSmileOutline,
