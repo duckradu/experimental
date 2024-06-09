7da2ca43bfc40cd74fef38de522fa20113a2fb97
2blackholes2 <radu.dascalu97@gmail.com>
2024-06-09 13:08:04 +0100

    Rename home page

---
+++ apps/monolith/src/routes/(platform)/(home).tsx
@@ -0,0 +1,11 @@
+import { PageTitle } from "~/components/page-title";
+
+export default function Home() {
+  return (
+    <main>
+      <PageTitle>Hello World</PageTitle>
+
+      <h1>Hello World - Home page</h1>
+    </main>
+  );
+}
