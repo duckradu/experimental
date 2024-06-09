bdb6fdbe2afb0addcee2b299f925526f8c90702a
2blackholes2 <radu.dascalu97@gmail.com>
2024-06-09 11:36:38 +0100

    Add new icons

--- apps/monolith/src/components/ui/icon.tsx
+++ apps/monolith/src/components/ui/icon.tsx
@@ -1,1 +1,11 @@
-export const Icon = {};
+import SolarHomeSmileBold from "~icons/solar/home-smile-bold";
+import SolarHomeSmileOutline from "~icons/solar/home-smile-outline";
+
+export const Icon = {
+  home: {
+    smile: {
+      outline: SolarHomeSmileOutline,
+      bold: SolarHomeSmileBold,
+    },
+  },
+};
