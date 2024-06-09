01d18d2ced96258a0b8a20fbbcf9c70ac94becaa
2blackholes2 <radu.dascalu97@gmail.com>
2024-06-09 11:37:19 +0100

    Refactor the 404 page

--- apps/monolith/src/routes/[...404].tsx
+++ apps/monolith/src/routes/[...404].tsx
@@ -1,15 +1,37 @@
-import { Title } from "@solidjs/meta";
+import { A } from "@solidjs/router";
 import { HttpStatusCode } from "@solidjs/start";
+
+import { PageTitle } from "~/components/page-title";
+import { SquigglesCentralFocusLayout } from "~/components/squiggles-central-content-layout";
+import { Icon } from "~/components/ui/icon";
 
 export default function NotFound() {
   return (
-    <main>
-      <Title>Not Found</Title>
+    <>
+      <PageTitle>Page not found</PageTitle>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit <a href="" target="_blank"></a>
-      </p>
-    </main>
+
+      <SquigglesCentralFocusLayout>
+        <main class="max-w-sm w-full text-center space-y-6">
+          <h1 class="text-9xl font-black tracking-wide">
+            40<span class="-scale-x-100 scale-y-100 inline-block">4</span>
+          </h1>
+
+          <div class="space-y-2">
+            <h2 class="text-2xl font-semibold tracking-tight">
+              Page not found
+            </h2>
+            <p class="text-muted-foreground">
+              The page you are looking for was moved, removed, renamed or might
+              have never existed!
+            </p>
+          </div>
+
+          <A href="/" class="inline-block text-2xl">
+            <Icon.home.smile.outline />
+          </A>
+        </main>
+      </SquigglesCentralFocusLayout>
+    </>
   );
 }
