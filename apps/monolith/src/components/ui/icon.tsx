d7b4b2a865f14616e7076c0c9466dec80aecfa37
2blackholes2 <radu.dascalu97@gmail.com>
2024-06-13 18:09:07 +0100

    Add spinner icon

--- apps/monolith/src/components/ui/icon.tsx
+++ apps/monolith/src/components/ui/icon.tsx
@@ -1,17 +1,20 @@
 import LogoSolid from "~icons/local/logo-solid";
+import SolarCompassBold from "~icons/solar/compass-bold";
+import SolarCompassOutline from "~icons/solar/compass-outline";
 import SolarHomeSmileBold from "~icons/solar/home-smile-bold";
 import SolarHomeSmileOutline from "~icons/solar/home-smile-outline";
-import SolarCompassOutline from "~icons/solar/compass-outline";
-import SolarCompassBold from "~icons/solar/compass-bold";
-import SolarShopOutline from "~icons/solar/shop-outline";
-import SolarShopBold from "~icons/solar/shop-bold";
-import SolarLoginOutline from "~icons/solar/login-outline";
 import SolarLoginBold from "~icons/solar/login-bold";
-import SolarUserPlusOutline from "~icons/solar/user-plus-outline";
+import SolarLoginOutline from "~icons/solar/login-outline";
+import SolarShopBold from "~icons/solar/shop-bold";
+import SolarShopOutline from "~icons/solar/shop-outline";
+import SolarTrafficLineDuotone from "~icons/solar/traffic-line-duotone";
 import SolarUserPlusBold from "~icons/solar/user-plus-bold";
+import SolarUserPlusOutline from "~icons/solar/user-plus-outline";
 
 export const Icon = {
   logo: { bold: LogoSolid },
+
+  spinner: SolarTrafficLineDuotone,
 
   home: {
     smile: {
