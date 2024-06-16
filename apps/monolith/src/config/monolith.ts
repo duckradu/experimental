de573ec581187dd87dc08a96363c56d057a198d8
2blackholes2 <radu.dascalu97@gmail.com>
2024-06-16 10:34:31 +0100

    Create initial seed script

--- apps/monolith/src/config/monolith.ts
+++ apps/monolith/src/config/monolith.ts
@@ -2,14 +2,20 @@ import { SessionConfig } from "vinxi/http";
 
 import { paths } from "~/lib/paths";
 
+export const INITIAL_ACTOR = {
+  pid: "kameleon",
+};
+
 export const appConfig = {
   name: "kameleon.social",
 
-  aboutUsRecordLink: paths.actor("kameleon").record("about-us").root,
-  termsAndPrivacyRecordLink: paths.actor("kameleon").record("terms-and-privacy")
+  aboutUsRecordLink: paths.actor(INITIAL_ACTOR.pid).record("about-us").root,
+  termsAndPrivacyRecordLink: paths
+    .actor(INITIAL_ACTOR.pid)
+    .record("terms-and-privacy").root,
+  helpRecordLink: paths.actor(INITIAL_ACTOR.pid).record("help").root,
+  sendFeedbackRecordLink: paths.actor(INITIAL_ACTOR.pid).record("send-feedback")
     .root,
-  helpRecordLink: paths.actor("kameleon").record("help").root,
-  sendFeedbackRecordLink: paths.actor("kameleon").record("send-feedback").root,
 };
 
 export const sessionConfig = {
--- apps/monolith/src/lib/db/seed.ts
+++ apps/monolith/src/lib/db/seed.ts
@@ -1,7 +1,29 @@
+import * as argon2 from "argon2";
+
+import { db } from "~/lib/db";
+import { actors } from "~/lib/db/schema";
+
 import { emptyTables } from "~/lib/utils/db";
+
+import { INITIAL_ACTOR } from "~/config/monolith";
 
 (async function seed() {
   await emptyTables();
+
+  await db
+    .insert(actors)
+    .values({
+      id: 0,
+      pid: INITIAL_ACTOR.pid,
+
+      name: "Test Account",
+      externalUrl: "https://kameleon.social",
+      // dob:
+
+      email: "test@kameleon.social",
+      encryptedPassword: await argon2.hash("password"),
+    })
+    .returning();
 
   process.exit(0);
 })();
