57df2c7b77a56abb14300b5199334e8a166bf8d4
2blackholes2 <radu.dascalu97@gmail.com>
2024-06-16 10:17:19 +0100

    Add route level redirect

--- apps/monolith/src/routes/(platform)/a/[a_pid]/settings/appearance.tsx
+++ apps/monolith/src/routes/(platform)/a/[a_pid]/settings/appearance.tsx
@@ -1,7 +1,23 @@
+import { Navigate, useParams } from "@solidjs/router";
+import { createRenderEffect } from "solid-js";
+
 import { SettingsHeader } from "~/components/settings";
 import { Separator } from "~/components/ui/separator";
 
+import { useAuth } from "~/providers/authentication";
+
+import { paths } from "~/lib/paths";
+
 export default function ThemeSettings() {
+  const { actor } = useAuth();
+  const params = useParams();
+
+  createRenderEffect(() => {
+    if (!actor()) {
+      return <Navigate href={paths.actor(params.a_pid).settings.root} />;
+    }
+  });
+
   return (
     <>
       <SettingsHeader
