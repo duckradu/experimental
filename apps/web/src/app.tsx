29cf01664365a3015a109906ac0b074cfc7b5397
duckradu <radu.dascalu@pm.me>
2024-07-16 20:18:44 +0100

    Add custom styles & links

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { For } from "solid-js";
 
 import "@unocss/reset/tailwind-compat.css";
 
@@ -7,25 +7,39 @@ import "virtual:uno.css";
 import "~/styles/app.css";
 
 export default function App() {
-  const [count, setCount] = createSignal(0);
-
   return (
-    <main>
-      <h1>Hello world!</h1>
-      <button
-        class="increment"
-        onClick={() => setCount(count() + 1)}
-        type="button"
-      >
-        Clicks: {count()}
-      </button>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+    <main class="flex items-center justify-center min-h-screen bg-gradient-to-b from-[#082f49] to-[#081924] text-white">
+      <section class="space-y-4 border-2 border-sky-950 p-4 rounded-2xl">
+        <header class="font-bold text-2xl">SolidStart app running!</header>
+
+        <div>
+          <h2 class="font-medium">Useful links to get started:</h2>
+
+          <ul>
+            <For
+              each={[
+                "https://github.com/duckradu/sprout",
+                "https://start.solidjs.com",
+                "https://github.com/thetarnav/solid-devtools",
+                "https://orm.drizzle.team",
+                "https://unocss.dev",
+              ]}
+            >
+              {(url) => (
+                <li>
+                  <a
+                    href={url}
+                    target="_blank"
+                    class="text-blue underline hover:no-underline"
+                  >
+                    {url}
+                  </a>
+                </li>
+              )}
+            </For>
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
