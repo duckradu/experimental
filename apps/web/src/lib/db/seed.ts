a88cd0ab9cf27f45ebd05404327ef958e45b4992
duckradu <radu.dascalu@pm.me>
2024-07-17 19:49:26 +0100

    Add initial actor seed

--- apps/web/src/lib/db/seed.ts
+++ apps/web/src/lib/db/seed.ts
@@ -1,9 +1,29 @@
+import * as argon2 from "argon2";
+
+import { db } from "~/lib/db";
+import { actors } from "~/lib/db/schema";
+
 import { emptyTables } from "~/lib/utils/db";
+
+import { INITIAL_ACTOR } from "~/config/app";
 
 (async function seed() {
   await emptyTables();
 
-  // Seed your tables here
+  await db
+    .insert(actors)
+    .values({
+      id: 0,
+      pid: INITIAL_ACTOR.pid,
+
+      name: "Test Account",
+      externalUrl: "https://kameleon.social",
+      // dob:
+
+      email: "test@kameleon.social",
+      encryptedPassword: await argon2.hash("password"),
+    })
+    .returning();
 
   process.exit(0);
 })();
